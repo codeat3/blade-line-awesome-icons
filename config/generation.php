21e8d85c175b2aa22ec6603bfb5d15a76ed26bde
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-07-09 20:50:39 +0530

    wip

--- config/generation.php
+++ config/generation.php
@@ -8,9 +8,6 @@ $svgNormalization = static function (string $tempFilepath, array $iconSet) {
     $iconProcessor = new IconProcessor($tempFilepath, $iconSet);
     $iconProcessor
         ->optimize()
-        ->postOptimizationAsString(function ($svgLine) {
-            return str_replace('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">', '', $svgLine);
-        })
         ->save();
 
 };
