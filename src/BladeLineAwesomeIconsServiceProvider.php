aad3b3d609b6f2dbecb96c09758a8f6f49bdf063
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:14:26 +0000

    Fix styling

--- src/BladeLineAwesomeIconsServiceProvider.php
+++ src/BladeLineAwesomeIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeLineAwesomeIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-line-awesome-icons', []);
 
-            $factory->add('line-awesome-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('line-awesome-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-line-awesome-icons.php', 'blade-line-awesome-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-line-awesome-icons.php', 'blade-line-awesome-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-line-awesome-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-line-awesome-icons'),
             ], 'blade-line-awesome-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-line-awesome-icons.php' => $this->app->configPath('blade-line-awesome-icons.php'),
+                __DIR__ . '/../config/blade-line-awesome-icons.php' => $this->app->configPath('blade-line-awesome-icons.php'),
             ], 'blade-line-awesome-icons-config');
         }
     }
